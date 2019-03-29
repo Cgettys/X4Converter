1821b3183803e7c93747787e7eb0b70d93923e6b
Charlie Gettys <charlie.gettys@comcast.net>
2019-03-28 23:00:51 -0400

    Needed by dead xac code

---
+++ X4ConverterTools/include/X4ConverterTools/Util/BinaryWriter.h
@@ -0,0 +1,34 @@
+#pragma once
+
+class BinaryWriter {
+public:
+    explicit BinaryWriter(Assimp::IOStream *pStream);
+
+    Assimp::IOStream *GetStream() const;
+
+    template<typename T>
+    void Write(const T &value) {
+        Write(&value, 1);
+    }
+
+    template<typename T>
+    void Write(const T *pValue, int count) {
+        _pStream->Write(pValue, sizeof(T), count);
+    }
+
+    template<typename T>
+    void Write(const std::vector<T> &values) {
+        if (!values.empty())
+            Write(values.data(), values.size());
+    }
+
+    template<typename T>
+    void Write(const std::string &str) {
+        Write<int>(str.size());
+        if (!str.empty())
+            Write(str.data(), str.size());
+    }
+
+private:
+    Assimp::IOStream *_pStream;
+};
---
+++ X4ConverterTools/src/Util/BinaryWriter.cpp
@@ -0,0 +1,9 @@
+#include <X4ConverterTools/StdInc.h>
+
+BinaryWriter::BinaryWriter(Assimp::IOStream *pStream) {
+    _pStream = pStream;
+}
+
+Assimp::IOStream *BinaryWriter::GetStream() const {
+    return _pStream;
+}
